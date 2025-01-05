3f55eeb5b08d71d5c29a2fdb8e70106002d7705f
Ben Brackenbury <benbrack22@icloud.com>
2025-01-05 20:50:20 +0000

    chore(web): set up prettier and .editorconfig

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,18 +1,20 @@
-import Link from "next/link";
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl">Store My Code</h1>
-      <p className="text-xl mt-2">
-        Follow progress on the{" "}
+    <div className='p-8'>
+      <h1 className='text-3xl'>Store My Code</h1>
+      <p className='mt-2 text-xl'>
+        Follow progress on the{' '}
         <Link
-          className='underline underline-offset-2 opacity-80 hover:opacity-100 transition-opacity duration-200'
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/benbrackenbury/storemycode"
-        >Github</Link>
+          className='underline underline-offset-2 opacity-80 transition-opacity duration-200 hover:opacity-100'
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://github.com/benbrackenbury/storemycode'
+        >
+          Github
+        </Link>
       </p>
     </div>
-  );
+  )
 }
